17b75c4a8f61fe0158da1836dfa5e7e2275e66f0
Ville <villeolofsgard@gmail.com>
2023-03-21 20:07:55 +0100

    Removed node remove test in counter

--- modules/Counter.svelte
+++ modules/Counter.svelte
@@ -12,18 +12,8 @@
 		count += 1;
 		_settings.Set('count', count);
 	}
-
-	let test = new DataStore('test');
-	let nodeRef: HTMLDivElement;
 </script>
   
 <button on:click={increment}>
 	count is {count}
 </button>
-
-<div bind:this={nodeRef}>
-	<h3>Hey I'm a component</h3>
-	<button on:click={() => nodeRef.parentNode.removeChild(nodeRef)}>
-	  Remove me
-	</button>
-  </div>
