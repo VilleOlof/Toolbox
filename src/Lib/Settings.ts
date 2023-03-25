40f7a59deb178a12a81141e4a61e6bec797dd7aa
Ville <villeolofsgard@gmail.com>
2023-03-25 19:19:47 +0100

    Fixed some TypeDoc config & Added a ton of docs and examples in Settings.ts

--- src/Lib/Settings.ts
+++ src/Lib/Settings.ts
@@ -15,6 +15,12 @@ namespace GlobalSettings {
     /**
      * Loads all the global settings.  
      * This should be called when the app starts.
+     * 
+     * **Example:**
+     * 
+     * ```ts
+     * GlobalSettings.LoadGlobalSettings();
+     * ```
      */
     export function LoadGlobalSettings(): void {
         _Settings = {};
@@ -30,6 +36,12 @@ namespace GlobalSettings {
      * Gets the settings for a component by ID in the GlobalSettings.
      * 
      * @param componentID The component ID.
+     * 
+     * **Example:**
+     * 
+     * ```ts
+     * let settings: Record<string, SettingTypes.Info> = GlobalSettings.GetComponentSettingsByID("ComponentID");
+     * ```
      */
     export function GetComponentSettingsByID(componentID: string): Record<string, SettingTypes.Info> {
         return GlobalSettings._Settings[componentID];
@@ -40,6 +52,13 @@ namespace GlobalSettings {
      * 
      * @param JSON The JSON string to save.
      * @param SettingsPath The path to the settings file.
+     * 
+     * **Example:**
+     * 
+     * ```ts
+     * let settingsJSON: string = JSON.stringify(GlobalSettings._Settings, null, 4);
+     * GlobalSettings.SaveToJSON(settingsJSON);
+     * ```
      */
     export function SaveToJSON(JSON: string, SettingsPath: string = "./../Settings.json"): void {
         const fs = require("fs");
@@ -55,6 +74,13 @@ namespace GlobalSettings {
      * 
      * @param componentID The component ID.
      * @param _Settings The settings to save.
+     * 
+     * **Example:**
+     * 
+     * ```ts
+     * let componentID: string = settings.GetComponentID();
+     * GlobalSettings.Save(componentID, settings);
+     * ```
      */
     export function Save(componentID: string, _Settings: Record<string, SettingTypes.Info>): void {
         GlobalSettings._Settings[componentID] = _Settings;
@@ -69,6 +95,12 @@ namespace GlobalSettings {
      * Including its instance.
      * 
      * @param SettingsInstance The settings instance to delete. 
+     * 
+     * **Example:**
+     * 
+     * ```ts
+     * GlobalSettings.DeleteComponentSettings(settings);
+     * ```
      */
     export function DeleteComponentSettings(SettingsInstance: Settings ): void {
         let componentID = SettingsInstance.GetComponentID();
@@ -93,16 +125,7 @@ namespace SettingTypes {
     /**
      * The type of setting.
      * 
-     * @param Text A text input.
-     * @param Numeric A numeric input.
-     * @param Checkbox A checkbox.
-     * @param Slider A slider.
-     * @param Dropdown A dropdown.
-     * @param Color A color picker.
-     * @param Date A date picker.
-     * @param RegExp A regular expression input.
-     * @param File A file prompt.
-     * @param Button A button.
+     * @enum
      */
     export enum Type {
         Text = 0, // String
@@ -116,6 +139,11 @@ namespace SettingTypes {
         File = 8, // String
         Button = 9, // Function
     }
+
+    /**
+     * The extra data types.
+     */
+    export type ExtraDataTypes = Slider | Dropdown | Button;
     
     /**
      * The setting info type.
@@ -129,7 +157,7 @@ namespace SettingTypes {
         Value: any;
         Type: Type;
         Description: string;
-        ExtraData?: Slider | Dropdown | Button;
+        ExtraData?: ExtraDataTypes;
     }
     
     /**
@@ -167,12 +195,24 @@ namespace SettingTypes {
 }
 
 /**
- * The settings class.
- * 
- * @param componentID The ID of the component that the settings are for.
+ * The settings class.  
+ * Used to Save/Load & Register settings for a component.  
+ * Registered settings will show up in the settings menu.  
  */
 class Settings {
 
+    /**
+     * The settings constructor.
+     * 
+     * @constructor
+     * @param componentID The ID of the component that the settings are for.
+     * 
+     * **Example:**
+     * 
+     * ```ts
+     * let settings: Settings = new Settings("ComponentID");
+     * ```
+     */
     constructor(componentID: string) {
         this._ComponentID = componentID;
 
@@ -200,8 +240,35 @@ class Settings {
      * @param type The type of the setting input
      * @param ExtraData Extra data for the setting type (Optional)
      * @param loadOldValues Whether to load old values from the settings file (Optional)
+     * 
+     * **Basic example:**
+     * 
+     * ```ts
+     *  settings.RegisterSetting("Increment", "The value to increment the count of", 1, SettingTypes.Type.Numeric);
+     * ```    
+     * 
+     * **Slider example:**
+     * 
+     * ```ts
+     *  settings.RegisterSetting("Default Zoom", "The default zoom level", 1.5, SettingTypes.Type.Slider, {
+            Min: 0.5,
+            Max: 2,
+            Step: 0.1
+        });
+     * ```
+     * 
+     * **Button example:**
+     * 
+     * ```ts
+     *  settings.RegisterSetting("Reset", "Resets the settings", null, SettingTypes.Type.Button, {
+            Label: "Reset",
+            Function: () => {
+                settings.DeleteAllLocalSettings();
+            }
+        }
+     * ```
      */
-    public RegisterSetting(settingName: string, settingDescription: string, defaultValue: any, type: SettingTypes.Type, ExtraData?: SettingTypes.Slider | SettingTypes.Dropdown | SettingTypes.Button, loadOldValues: boolean = true) {
+    public RegisterSetting(settingName: string, settingDescription: string, defaultValue: any, type: SettingTypes.Type, ExtraData?: SettingTypes.ExtraDataTypes, loadOldValues: boolean = true) {
         
         //a bit jank, copilot generated and had issues with loading old values before. but this works
         if (loadOldValues) {
@@ -229,6 +296,12 @@ class Settings {
      * 
      * @param settingName The name of the setting
      * @param value The value to set the setting to
+     * 
+     * **Example:**
+     * 
+     * ```ts
+     * settings.Set("Increment", 2);
+     * ```
      */
     public Set(settingName: string, value: any) {
         this._Settings[settingName].Value = value;
@@ -240,6 +313,12 @@ class Settings {
      * Gets the setting info for specified setting.
      * 
      * @param settingName The name of the setting
+     * 
+     * **Example:**
+     * 
+     * ```ts
+     * let settingInfo: SettingTypes.Info = settings.GetSettingInfo("Increment");
+     * ```
      */
     public GetSettingInfo(settingName: string): SettingTypes.Info {
         return this._Settings[settingName];
@@ -250,13 +329,25 @@ class Settings {
      * 
      * @param settingName The name of the setting
      * @param defaultValue The default value to return if the setting doesn't exist (Optional)
+     * 
+     * **Example:**
+     * 
+     * ```ts
+     * let increment: number = settings.GetSettingValue<number>("Increment");
+     * ```
      */
-    public GetSettingValue(settingName: string, defaultValue?: any): any {
+    public GetSettingValue<T>(settingName: string, defaultValue?: T): T {
         return this._Settings[settingName]?.Value || defaultValue;
     }
 
     /**
      * Gets all the settings for the component.
+     * 
+     * **Example:**
+     * 
+     * ```ts
+     * let allSettings: Record<string, SettingTypes.Info> = settings.GetAllComponentSettings();
+     * ```
      */
     public GetAllComponentSettings(): Record<string, SettingTypes.Info> {
         return this._Settings;
@@ -264,6 +355,12 @@ class Settings {
 
     /**
      * Gets the component ID of the current instance.
+     * 
+     * **Example:**
+     * 
+     * ```ts
+     * let componentID: string = settings.GetComponentID();
+     * ```
      */
     public GetComponentID(): string {
         return this._ComponentID;
@@ -273,6 +370,14 @@ class Settings {
      * Loads the settings from "JSON".
      * 
      * @param settingData The setting data to load
+     * 
+     * **Example:**
+     * 
+     * ```ts
+     * settings.Load({
+     *  Increment: 4
+     * });
+     * ```
      */
     public Load(settingData: any): void {
         for (let key in settingData) {
@@ -284,6 +389,12 @@ class Settings {
      * Deletes a setting.
      * 
      * @param settingName The name of the setting 
+     * 
+     * **Example:**
+     * 
+     * ```ts
+     * settings.DeleteSetting("Increment");
+     * ```
      */
     public DeleteSetting(settingName: string): void {
         delete this._Settings[settingName];
@@ -293,6 +404,12 @@ class Settings {
 
     /**
      * Deletes all the settings for the component.
+     * 
+     * **Example:**
+     * 
+     * ```ts
+     * settings.DeleteAllLocalSettings();
+     * ```
      */
     public DeleteAllLocalSettings(): void {
         delete this._Settings
