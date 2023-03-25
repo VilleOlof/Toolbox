89d737559c03c505d95bd22bff77c3deaf6b4e80
Ville <villeolofsgard@gmail.com>
2023-03-25 23:01:14 +0100

    Added more detailed docs to DataStore.ts

--- src/Stores/DataStore.ts
+++ src/Stores/DataStore.ts
@@ -9,13 +9,23 @@ let _DataStores: Record<string, DataStore> = {};
 type CallbackFunction = (value: any) => void;
 
 /**
- * A simple data store that can be used to store and retrieve data for a component.
- * 
- * @param componentID The ID of the component that the data is for.
- * @param loadUponStart Whether or not to load the data upon creation of the store.
+ * A simple data store that can be used to store and retrieve various data for a component.
+ * Saved in local storage.
  */
 class DataStore {
 
+    /**
+     * Creates a new data store for a component.
+     * 
+     * @constructor
+     * @param componentID The ID of the component that the data is for.
+     * @param loadUponStart Whether or not to load the data upon creation of the store.
+     * 
+     * @example
+     * ```ts
+     * const DataStore = new DataStore("ComponentID");
+     * ```
+     */
     constructor(componentID: string, loadUponStart: boolean = true) {
         this._ComponentID = componentID;
 
@@ -48,6 +58,13 @@ class DataStore {
      * 
      * @param key The key of the data that has changed.
      * @param value The new value of the data.
+     * 
+     * @private
+     * 
+     * @example
+     * ```ts
+     * this.Notify("Key", "Value");
+     * ```
      */
     private Notify(key: string, value: any): void {
         if (this._Subscribers[key]) {
@@ -67,6 +84,11 @@ class DataStore {
      * @param value The value of the data to set.
      * @param notify Whether or not to notify subscribers of the change.
      * @param save Whether or not to save the data. If SaveUponChange is true, this will be ignored.
+     * 
+     * @example
+     * ```ts
+     * this.Set("Key", "Value");
+     * ```
      */
     public Set(key: string, value: any = undefined, notify: boolean = true, save: boolean = true): void {
         this._Data[key] = value;
@@ -79,13 +101,23 @@ class DataStore {
      * 
      * @param key The key of the data to get.
      * @param defaultValue The default value to return if the key does not exist.
+     * 
+     * @example
+     * ```ts
+     * const Value: T = this.Get<T>("Key");
+     * ```
      */
-    public Get<T>(key: string, defaultValue: any = undefined): T {
+    public Get<T>(key: string, defaultValue?: T): T {
         return this._Data[key] || defaultValue;
     };
 
     /**
      * Loads the data from local storage.
+     * 
+     * @example
+     * ```ts
+     * this.Load();
+     * ```
      */
     public Load(): void {
         const settings = localStorage.getItem(this._ComponentID);
@@ -96,6 +128,11 @@ class DataStore {
 
     /**
      * Saves the data to local storage.
+     * 
+     * @example
+     * ```ts
+     * this.Save();
+     * ```
      */
     public Save(): void {
         localStorage.setItem(this._ComponentID, JSON.stringify(this._Data));
@@ -106,6 +143,11 @@ class DataStore {
      * 
      * @param key The key of the data to delete.
      * @param notify Whether or not to notify subscribers of the change.
+     * 
+     * @example
+     * ```ts
+     * this.Delete("Key");
+     * ```
      */
     public Delete(key: string, notify: boolean = false) {
         localStorage.removeItem(this._ComponentID);
@@ -121,8 +163,14 @@ class DataStore {
      * 
      * @param key The key to subscribe to.
      * @param callback The callback to call when the data changes.
+     * 
+     * @example
+     * ```ts
+     * this.Subscribe<T>("Key", (value: T) => {
+     *    // Do something with the value.
+     * });
      */
-    public Subscribe(key: string, callback: (value: any) => void) {
+    public Subscribe<T>(key: string, callback: (value: T) => void) {
         if (!this._Subscribers[key]) {
             this._Subscribers[key] = [];
         }
@@ -134,6 +182,12 @@ class DataStore {
      * 
      * @param key The key to unsubscribe from.
      * @param callback The callback to unsubscribe.
+     * 
+     * @example
+     * ```ts
+     * this.Unsubscribe<T>("Key", (value: T) => {
+     *   // Do something with the value.
+     * });
      */
     public Unsubscribe(key: string, callback: (value: any) => void) {
         if (this._Subscribers[key]) {
@@ -143,6 +197,11 @@ class DataStore {
 
     /**
      * Gets the ID of the component that the data is for.
+     * 
+     * @example
+     * ```ts
+     * const ComponentID: string = this.GetComponentID();
+     * ```
      */
     public GetComponentID(): string {
         return this._ComponentID;
@@ -152,6 +211,11 @@ class DataStore {
      * Gets the data store for a component ID.
      * 
      * @param componentID The ID of the component to get the data store for.
+     * 
+     * @example
+     * ```ts
+     * const dataStore: DataStore = DataStore.GetDataStoreByID("ComponentID");
+     * ```
      */
     public static GetDataStoreByID(componentID: string): DataStore {
         return _DataStores[componentID];
@@ -159,6 +223,11 @@ class DataStore {
 
     /**
      * Gets all the data stores.
+     * 
+     * @example
+     * ```ts
+     * const dataStores: Record<string, DataStore> = DataStore.GetAllDataStores();
+     * ```
      */
     public static GetAllDataStores(): Record<string, DataStore> {
         return _DataStores;
@@ -170,6 +239,13 @@ class DataStore {
      * @param componentID The ID of the component to delete the data store for.
      * @param DataStoreInstance The data store to delete.
      * @param input The input to delete the data store for.
+     * 
+     * @example
+     * ```ts
+     * DataStore.DeleteDataStore("ComponentID");
+     * // OR
+     * DataStore.DeleteDataStore(dataStore);
+     * ```
      */
     public static DeleteDataStore(componentID: string): void
     public static DeleteDataStore(DataStoreInstance: DataStore): void
