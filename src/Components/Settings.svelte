d0ec68e9af9d3745e6e86099a2b5526ee28f7a7a
Ville <villeolofsgard@gmail.com>
2023-03-29 02:14:34 +0200

    removed data.json

--- src/Components/Settings.svelte
+++ src/Components/Settings.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+    import { onMount } from "svelte";
     import { GlobalSettings, SettingTypes, Settings } from "../Lib/Settings";
 
     let InputComponents: Record<string, Function> = {};
